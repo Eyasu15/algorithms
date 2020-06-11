b39bcc81d649ae9e5e0a2c18e4cbae5c69e0488b
Eyasu Weld <Eyasuweld@gmail.com>
2020-06-10 22:14:11 -0500

    Counting Sort implementation completer.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,16 +2,22 @@ package main;
 
 import java.util.Arrays;
 
+import counting.CountingSort;
 import quick.QuickSort;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int[] nums = { 4, 3, 2, 1 };
+//		int[] nums = { 4, 3, 2, 1 };
+//		
+//		Sorter sort = new QuickSort();
+//		sort.sort(nums);
+//		System.out.println(Arrays.toString(nums));
 		
-		Sorter sort = new QuickSort();
-		sort.sort(nums);
-		System.out.println(Arrays.toString(nums));
+		int[] arr = {12, 15, 19, 18, 12, 18, 17, 20, 14, 13};
+		var count = new CountingSort();
+		count.sort(arr, 10, 20);
+		System.out.println(Arrays.toString(arr));
 	}
 	
 
